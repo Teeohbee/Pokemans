18a20c12e67bad01c2eba7db22798ed044d4d9d2
Toby Clarke <teeohbee@gmail.com>
2023-10-03 20:58:11 +0100

    Dynamically create combat actions

--- CombatActionButton.gd
+++ CombatActionButton.gd
@@ -3,4 +3,4 @@ extends Button
 var combat_action : CombatAction
 
 func _on_pressed():
-	get_node("/root/BattleScene").current_character.cast_combat_action(combat_action)
+	get_node("/root/BattleScene").current_char.cast_combat_action(combat_action)
--- PlayerUI.gd
+++ PlayerUI.gd
@@ -8,6 +8,20 @@ func _ready():
 
 func _on_character_begin_turn(character):
 	visible = character.is_player
-
+	
+	if character.is_player:
+		_display_combat_actions(character.combat_actions)
+		
 func _on_character_end_turn(character):
 	visible = false
+
+func _display_combat_actions(combat_actions):
+	for i in len(buttons):
+		var button = get_node(buttons[i])
+
+		if i < len(combat_actions):
+			button.visible = true
+			button.text = combat_actions[i].display_name
+			button.combat_action = combat_actions[i]
+		else:
+			button.visible = false
